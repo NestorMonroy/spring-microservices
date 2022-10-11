6fb5cc28b1dc88a96f164abd8fc86381a04e847d
NestorMonroy <nestor.monroy.90@gmail.com>
2022-10-11 00:01:41 -0400

    [add] example limits server

---
+++ 02.microservices/limits-service/src/test/java/mx/nestor/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.nestor.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
